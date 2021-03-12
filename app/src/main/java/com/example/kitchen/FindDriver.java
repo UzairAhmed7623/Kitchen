6e8c43190f238fd3d0bd14eb218e943037c14a4a
UzairAhmed7623 <hafiz.uzair579@gmail.com>
2021-03-12 17:22:11 +0500

    commit 3!

--- app/src/main/java/com/example/kitchen/FindDriver.java
+++ app/src/main/java/com/example/kitchen/FindDriver.java
@@ -1,20 +1,45 @@
 package com.example.kitchen;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.TransportMode;
+import com.akexorcist.googledirection.model.Direction;
+import com.bumptech.glide.Glide;
 import com.firebase.geofire.GeoFire;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -28,10 +53,20 @@ import org.imperiumlabs.geofirestore.GeoQuery;
 import org.imperiumlabs.geofirestore.listeners.GeoQueryEventListener;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class FindDriver extends FragmentActivity implements OnMapReadyCallback {
 
+    private CircleImageView driverImage;
+    private TextView driverName,driverPhone;
+    private LinearLayout layout;
     private GoogleMap mMap;
     private FirebaseFirestore firebaseFirestore;
+
+    private LatLng latLng;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +79,12 @@ public class FindDriver extends FragmentActivity implements OnMapReadyCallback {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(FindDriver.this);
 
+        driverImage = (CircleImageView) findViewById(R.id.driverImage);
+        driverName = (TextView) findViewById(R.id.driverName);
+        driverPhone = (TextView) findViewById(R.id.driverPhone);
+        layout = (LinearLayout) findViewById(R.id.layout);
 
     }
 
@@ -53,15 +93,219 @@ public class FindDriver extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        getClosestDriver();
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+
+                    latLng = new LatLng(latitude, longitude);
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("You"));
+                    getClosestCustomer(latLng);
+
+                }
+                else {
+                    Toast.makeText(FindDriver.this, "Null", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 
-    private void getClosestDriver(){
+    private void getClosestCustomer(LatLng latLng) {
+//        String id = "cb0xbVIcK5dWphXuHIvVoUytfaM2";
+//        CollectionReference collectionReference = firebaseFirestore.collection("Users");
+//        GeoFirestore geoFirestore = new GeoFirestore(collectionReference);
+//        geoFirestore.setLocation(id, new GeoPoint(latLng.latitude, latLng.longitude));
+
+        getClosestDriver(latLng);
+    }
+
+
+    private int radius = 1;
+    private boolean driverFound = false;
+    private String driverFoundID;
+
+    private void getClosestDriver(LatLng latLng){
+
         CollectionReference collectionReference = firebaseFirestore.collection("Users");
+
+        Log.d("TAG", "LatLng"+latLng);
+
+        GeoFirestore geoFirestore = new GeoFirestore(collectionReference);
+        GeoQuery geoQuery = geoFirestore.queryAtLocation(new GeoPoint(latLng.latitude,latLng.longitude),radius);
+        geoQuery.removeAllListeners();
+
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(@NotNull String key, @NotNull GeoPoint geoPoint) {
+                if (!driverFound){
+                    driverFound = true;
+                    Log.d("TAG","Boolean: "+driverFound);
+
+                    driverFoundID = key;
+                    Log.d("TAG","Driver: "+driverFoundID);
+                    Log.d("TAG","Driver's GeoPoints: "+geoPoint);
+
+                    firebaseFirestore.collection("Users").document(key).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()){
+                                DocumentSnapshot documentSnapshot = task.getResult();
+                                if (documentSnapshot.exists()){
+                                    String fName = documentSnapshot.getString("First Name");
+                                    String lName = documentSnapshot.getString("Last Name");
+                                    String phone = documentSnapshot.getString("Phone");
+                                    String imageUri = documentSnapshot.getString("imageProfile");
+
+                                    driverName.setText(fName + " " + lName);
+                                    driverPhone.setText(phone);
+                                    Glide.with(FindDriver.this).load(imageUri).placeholder(R.drawable.driver_placeholder).fitCenter().into(driverImage);
+
+                                    LatLng latLng1 = new LatLng(geoPoint.getLatitude(),geoPoint.getLongitude());
+                                    mMap.addMarker(new MarkerOptions().position(latLng1).title(fName + " " + lName));
+//                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng1));
+                                    LatLngBounds.Builder latlngBuilder = new LatLngBounds.Builder();
+
+                                    latlngBuilder.include(latLng);
+                                    latlngBuilder.include(latLng1);
+
+                                    mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(latlngBuilder.build(), 100));
+
+                                    GoogleDirection.withServerKey("AIzaSyBa4XZ09JsXD8KYZr5wdle--0TQFpfyGew")
+                                            .from(latLng)
+                                            .to(latLng1)
+                                            .execute(new DirectionCallback() {
+                                                @Override
+                                                public void onDirectionSuccess(@Nullable Direction direction) {
+//                                                PolylineOptions polylineOptions = new PolylineOptions();
+//                                                polylineOptions.add()
+//                                                polylineOptions.color(R.color.purple_700);
+//                                                polylineOptions.width(2);
+                                                    Log.d("TAG222", ""+direction.getStatus());
+                                                    Log.d("TAG222", ""+direction.describeContents());
+                                                    Log.d("TAG222", ""+direction.getGeocodedWaypointList());
+                                                    Log.d("TAG222", ""+direction.getRouteList());
+
+                                                }
+
+                                                @Override
+                                                public void onDirectionFailure(Throwable t) {
+                                                    // Do something here
+                                                }
+                                            });
+
+                                    layout.setVisibility(View.VISIBLE);
+
+                                }
+                            }
+                        }
+                    });
+                }
+//                geoFirestore.removeLocation(driverFoundID);
+
+
+
+            }
+
+            @Override
+            public void onKeyExited(@NotNull String key) {
+
+            }
+
+            @Override
+            public void onKeyMoved(@NotNull String key, @NotNull GeoPoint geoPoint) {
+                firebaseFirestore.collection("Users").document(key).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            if (documentSnapshot.exists()){
+                                String fName = documentSnapshot.getString("First Name");
+                                String lName = documentSnapshot.getString("Last Name");
+                                String phone = documentSnapshot.getString("Phone");
+                                String imageUri = documentSnapshot.getString("imageProfile");
+
+                                driverName.setText(fName + " " + lName);
+                                driverPhone.setText(phone);
+                                Glide.with(FindDriver.this).load(imageUri).placeholder(R.drawable.driver_placeholder).fitCenter().into(driverImage);
+
+                                LatLng latLng1 = new LatLng(geoPoint.getLatitude(),geoPoint.getLongitude());
+                                mMap.addMarker(new MarkerOptions().position(latLng1).title(fName + " " + lName));
+//                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng1));
+                                LatLngBounds.Builder latlngBuilder = new LatLngBounds.Builder();
+
+                                latlngBuilder.include(latLng);
+                                latlngBuilder.include(latLng1);
+                                mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(latlngBuilder.build(), 1));
+
+
+
+
+                                GoogleDirection.withServerKey("AIzaSyBa4XZ09JsXD8KYZr5wdle--0TQFpfyGew")
+                                        .from(latLng)
+                                        .to(latLng1)
+                                        .execute(new DirectionCallback() {
+                                            @Override
+                                            public void onDirectionSuccess(@Nullable Direction direction) {
+//                                                PolylineOptions polylineOptions = new PolylineOptions();
+//                                                polylineOptions.add()
+//                                                polylineOptions.color(R.color.purple_700);
+//                                                polylineOptions.width(2);
+                                                Log.d("TAG222", ""+direction.getStatus());
+                                                Log.d("TAG222", ""+direction.describeContents());
+                                                Log.d("TAG222", ""+direction.getGeocodedWaypointList());
+                                                Log.d("TAG222", ""+direction.getRouteList());
+
+                                            }
+
+                                            @Override
+                                            public void onDirectionFailure(Throwable t) {
+                                                // Do something here
+                                            }
+                                        });
+
+
+
+
+
+
+
+                                layout.setVisibility(View.VISIBLE);
+
+                            }
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                Log.d("TAG","onGeoQuery");
+
+                if (!driverFound){
+                    radius++;
+                    getClosestDriver(latLng);
+                }
+            }
+
+            @Override
+            public void onGeoQueryError(@NotNull Exception e) {
+
+            }
+        });
+
         collectionReference.whereEqualTo("status","available").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
@@ -73,34 +317,7 @@ public class FindDriver extends FragmentActivity implements OnMapReadyCallback {
                         Double lat = documentSnapshot.getDouble("Location.latitude");
                         Double lng = documentSnapshot.getDouble("Location.longitude");
 
-                        GeoFirestore geoFirestore = new GeoFirestore(collectionReference);
-                        GeoQuery geoQuery = geoFirestore.queryAtLocation(new GeoPoint(31.4968802,74.3622108),1);
-                        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
-                            @Override
-                            public void onKeyEntered(@NotNull String s, @NotNull GeoPoint geoPoint) {
 
-                            }
-
-                            @Override
-                            public void onKeyExited(@NotNull String s) {
-
-                            }
-
-                            @Override
-                            public void onKeyMoved(@NotNull String s, @NotNull GeoPoint geoPoint) {
-
-                            }
-
-                            @Override
-                            public void onGeoQueryReady() {
-
-                            }
-
-                            @Override
-                            public void onGeoQueryError(@NotNull Exception e) {
-
-                            }
-                        });
 
                     }
                 }
